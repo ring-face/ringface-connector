307ac7ca1320e76684dbfa5e6731477021c49de4
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-22 09:13:58 +0100

    ensure dir structure exists

--- ringConnector/dirStructure.py
+++ ringConnector/dirStructure.py
@@ -1,4 +1,5 @@
 from decouple import config
+from pathlib import Path
 
 DATA_DIR = config('DATA_DIR')
 
@@ -6,5 +7,7 @@ DATA_DIR = config('DATA_DIR')
 class DirStructure():
     def __init__(self, dataDir = DATA_DIR):
         self.videos = dataDir + "/videos"
+        Path(self.videos).mkdir(parents=True, exist_ok=True)
+
 
 DEFAULT_DIR_STUCTURE = DirStructure()
