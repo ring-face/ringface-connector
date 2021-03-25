f1850f23f81f982a1c1ddbeeeed99ca6c6b6c2ef
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-25 09:09:39 +0100

    configurable docker builders

--- dockerImagesBuild.sh
+++ dockerImagesBuild.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 docker build . --tag ringface/connector:latest
 docker build -f Dockerfile.createauth . --tag ringface/createauth:latest
+
+if [ -z ${1+x} ]
+  then
+  echo "semver is not defined"
+  else
+  echo "tagging to '$1'"
+  docker tag ringface/connector:latest ringface/connector:$1
+  docker tag ringface/createauth:latest ringface/createauth:$1
+fi
--- dockerImagesPush.sh
+++ dockerImagesPush.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 docker push ringface/connector:latest
 docker push ringface/createauth:latest
+
+if [ -z ${1+x} ]
+  then
+  echo "semver is not defined"
+  else
+  echo "tagging to '$1'"
+    docker push ringface/connector:$1
+    docker push ringface/createauth:$1
+fi
