523492a9ad45bbbd7223b73f355304b3a5c00be6
abaxsoraszem <abax.soraszem@gmail.com>
2023-12-15 16:46:12 +0100

    function without internet ingress

--- deployFunctionToGcp.sh
+++ deployFunctionToGcp.sh
@@ -2,6 +2,7 @@ gcloud functions deploy download-ring-video \
 --gen2 \
 --project ringface-2 \
 --service-account connector-sa@ringface-2.iam.gserviceaccount.com \
+--ingress-settings internal-and-gclb \
 --runtime python310 \
 --source=. \
 --entry-point=download \
